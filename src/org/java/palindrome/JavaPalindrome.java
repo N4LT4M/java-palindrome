a030e92736057f004451f934f6e7c4ac7d80bade
N4LT4M <m4tt3o.casale@gmail.com>
2023-07-17 15:37:40 +0200

    fino al ciclo for

--- src/org/java/palindrome/JavaPalindrome.java
+++ src/org/java/palindrome/JavaPalindrome.java
@@ -1,4 +1,36 @@
 package org.java.palindrome;
 
+import java.util.Scanner;
+
 public class JavaPalindrome {
+    public static void main(String[] args) {
+        /*scrivere un programma che chiede all’utente di inserire una parola
+         e gli risponde se è una parola palindroma oppure no.
+         Una parola palindroma è una parola che si può leggere sia da sinistra verso destra che da destra verso sinistra e mantiene stesso suono e lo stesso significato.
+         Ad esempio: OTTO, RADAR, ESOSE
+         */
+
+
+        Scanner scan = new Scanner(System.in);
+        //chiedo all'utente una parola
+        String userWord;
+        System.out.println("scrivi una parola e verificherò se è palindroma");
+        userWord = scan.nextLine();
+        //creo un array dalla parola dell'utente
+        char[] arrayUserWord = userWord.toCharArray();
+        //inverto la parola
+        for (int i = arrayUserWord.length - 1; i >= 0 ; i--) {
+
+        }
+
+
+
+
+
+
+
+
+
+
+    }
 }
